de4c57d68dbab7bf8a17b461224c051963e62112
Luke Monroe <monroe.luke36@gmail.com>
2025-01-10 10:31:50 -0500

    added main controller for student/instructor webPages, started controller for OAuth2 but need some brightSpace credentials, added instructor and student.html pages and added local DB info in application.properties for testing. Added securityConfig File that stops any Authentication from being needed as of now for testing

---
+++ src/test/java/edu/pui/peerEvaluation/Peerevualuationapplication/PeerevualuationapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pui.peerEvaluation.Peerevualuationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerevualuationapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
